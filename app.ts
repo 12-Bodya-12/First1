3ba60d28ba217be065128a7c311b44073a158b8f
12-Bodya-12 <68699299+12-Bodya-12@users.noreply.github.com>
2020-11-17 14:28:47 +0300

    sr_0.0.1

--- app.ts
+++ app.ts
@@ -1,27 +1,72 @@
 const http = require('http')
 const fs = require('fs')
+const path = require('path')
+
+let adress
+let format
 
 console.log('server started et : http://localhost:3000/')
 
 http.createServer((req,res)=>{
-    if ((req.url==='/') || (req.url==='/index.html'))
-    fs.readFile(process.cwd() + '/index.html', 'UTF-8',(error,data)=>{
-        if (error) throw error
-            res.writeHead(200,{'Content-type':'text/html'})
-                res.end(data)
+
+    // if (req.url==='/')
+    //     fs.readFile(process.cwd() + '/index.html', 'UTF-8',(error,data)=>{
+    //         if (error) throw error
+    //             res.writeHead(200,{'Content-type':'text/html'})
+    //                 res.end(data)
+    //     })
+
+    //     if (req.url==='/style.css')
+    //     fs.readFile(process.cwd() + '/style.css', 'UTF-8',(error,data)=>{
+    //         if (error) throw error
+    //             res.writeHead(200,{'Content-type':'text/css'})
+    //                 res.end(data)
+    //     })
+
+    if (req.url === '/'){
+        req.url = '/index.html'
+        format = 'text/css'
+    }      
+     adress = path.extname(req.url)
+
+     switch(adress){
+        case ".html":
+             format = "text/html"
+             break;
+        case ".css":
+            format = "text/css"
+            break
+        case ".jpg":
+            format = "image/jpg"
+            break
+        case ".jpeg":
+            format = "image/jpeg"
+            break
+        case ".webp":
+            format = "image/webp"
+            break
+        case ".png":
+            format = "image/png"
+            break
+        case "":
+            format = "text/plain"
+            break
+        default : format = 'text/html'
+     }
+
+     fs.readFile(process.cwd() + req.url, (err, data) => {
+        if (err)
+            fs.readFile(process.cwd() + '/public/404.html', 'utf8', (err, data) => {
+                if (err) throw err
+                res.writeHead(404, { 'Content-type': 'text/html' })
+                res.end(data, 'utf-8')
+            })
+        else {
+            res.writeHead(200, { 'Content-type': format })
+            res.end(data)
+        }
     })
 
-    if (req.url==='/style.css')
-    fs.readFile(process.cwd() + '/style.css', 'UTF-8',(error,data)=>{
-        if (error) throw error
-            res.writeHead(200,{'Content-type':'text/css'})
-                res.end(data)
-    })
-
-    if (req.url==='/public/2.html')
-    fs.readFile(process.cwd() + '/public/2.html', 'UTF-8',(error,data)=>{
-        if (error) throw error
-            res.writeHead(200,{'Content-type':'text/html'})
-                res.end(data)
-    })
-}).listen(3000)
+}).listen(3000), ()=>{
+    console.log("Сервер запущен по http://localhost:3000");
+}
